c665aad4d8386f5ccddb902ff6948dcbfa4f86f2
https://github.com/ChickenNFries <gracealdavis@gmail.com>
2026-04-14 11:54:51 +0100

    Fix backend formatting

--- backend.tf
+++ backend.tf
@@ -8,10 +8,10 @@ terraform {
 
 
   backend "s3" {
-    bucket         = "winx-terraform-state"
-    key            = "global/terraform.tfstate"
-    region         = "eu-west-2"
+    bucket = "winx-terraform-state"
+    key    = "global/terraform.tfstate"
+    region = "eu-west-2"
     # dynamodb_table = "winx-terraform-locks"
-    encrypt        = true
+    encrypt = true
   }
 }
